bfd8ba396f4d9e5c773fba413c892e0a2be779c1
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2024-04-27 13:32:07 -0300

    chore: add eslint rules for main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from '@/app.module'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap()
